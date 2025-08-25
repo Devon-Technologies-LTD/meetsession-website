977fd07faea8648aa87e2ba0451570c8104e0ce2
Godswill Nasara <godswillnlm@gmail.com>
2025-08-25 21:50:24 +0100

    feat: modify navbar and how it works sections

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,6 +1,9 @@
+import { DownloadApps } from "@/features/base/components/download-apps";
+import { FAQs } from "@/features/base/components/faqs";
 import { Hero } from "@/features/base/components/hero";
 import { HowItWorks } from "@/features/base/components/how-it-works";
 import { JoinWaitlist } from "@/features/base/components/join-waitlist";
+import { Testimonials } from "@/features/base/components/testimonials";
 import { WhyMeetSession } from "@/features/base/components/why-meet-session";
 
 export default function Page() {
@@ -10,6 +13,9 @@ export default function Page() {
       <WhyMeetSession />
       <HowItWorks />
       <JoinWaitlist />
+      <Testimonials />
+      <DownloadApps />
+      <FAQs />
     </div>
   );
 }
