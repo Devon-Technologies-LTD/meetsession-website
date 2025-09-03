f787692c967572bb67d516bdb8998ed47b025a8f
Godswill Nasara <godswillnlm@gmail.com>
2025-09-03 01:54:43 +0100

    chore: reorganise codebase

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,10 +1,10 @@
 import { DownloadApps } from "@/features/base/components/download-apps";
-// import { FAQs } from "@/features/base/components/faqs";
 import { Hero } from "@/features/base/components/hero";
 import { HowItWorks } from "@/features/base/components/how-it-works";
 import { JoinWaitlist } from "@/features/base/components/join-waitlist";
-// import { Testimonials } from "@/features/base/components/testimonials";
 import { WhyMeetSession } from "@/features/base/components/why-meet-session";
+// import { Testimonials } from "@/features/base/components/testimonials";
+// import { FAQs } from "@/features/base/components/faqs";
 
 export default function Page() {
   return (
