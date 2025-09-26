155787f4b6a5a76c95381701f615702609c1dea9
Godswill Nasara <godswillnlm@gmail.com>
2025-09-26 12:16:53 +0100

    feat: setup middleware route management

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,57 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  // TODO: handle route management for auth paths
+import { createAuthService } from "./lib/auth-service";
+import { ALGORITHM, SECRET_KEY } from "./lib/constants";
+import { setServerCookie } from "./server/set-cookie";
+import {
+  apiPrefix,
+  authPaths,
+  DEFAULT_DASHBOARD_REDIRECT,
+  DEFAULT_LOGIN_REDIRECT,
+  protectedPaths,
+} from "./routes";
+
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  console.log("middleware: ", pathname);
-  // Middleware logic goes here
-  return NextResponse.next();
+  const res = NextResponse.next();
+
+  const auth = createAuthService({
+    secret: SECRET_KEY,
+    algorithm: ALGORITHM,
+    setCookie: setServerCookie,
+  });
+
+  const { getAccessToken } = auth;
+  const token = await getAccessToken();
+
+  const isLoggedIn = !!token;
+
+  const isApiRoute = pathname.startsWith(apiPrefix);
+  const isProtectedRoute = protectedPaths.some((route) => {
+    return pathname.startsWith(route);
+  });
+  const isAuthRoute = authPaths.some((route) => {
+    return pathname.startsWith(route);
+  });
+
+  if (isApiRoute) return res;
+
+  if (isAuthRoute) {
+    if (isLoggedIn)
+      return NextResponse.redirect(
+        new URL(DEFAULT_DASHBOARD_REDIRECT, request.nextUrl),
+      );
+    return res;
+  }
+
+  if (isProtectedRoute) {
+    if (isLoggedIn) return res;
+    return NextResponse.redirect(
+      new URL(DEFAULT_LOGIN_REDIRECT, request.nextUrl),
+    );
+  }
+
+  return res;
 }
 
 export const config = {
