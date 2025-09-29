15638846c952018a30852f4a48b1e1b2c04515de
Godswill Nasara <godswillnlm@gmail.com>
2025-09-29 02:27:07 +0100

    feat: implement post payment flow

--- src/features/dashboard/lib/server/actions.ts
+++ src/features/dashboard/lib/server/actions.ts
@@ -4,6 +4,10 @@ import { createApiClient } from "@/lib/api-client";
 import { BASE_URL } from "@/lib/constants";
 import { TSubscriptionPlan } from "@/lib/types";
 import { TQueryParams } from "@/lib/types";
+import { updateProfileSchema } from "../schemas";
+import { TProfile } from "../types";
+import { revalidatePath } from "next/cache";
+import z from "zod";
 
 const apiClient = createApiClient({
   baseURL: BASE_URL,
@@ -40,6 +44,53 @@ export async function retrievePlansAction(params?: retrievePlansParams) {
       errors: null,
       message: "Successfully retrieved plans",
       status: res.status,
+    };
+  }
+}
+
+export async function initiatePaymentAction(formdata: FormData) {
+  const data = Object.fromEntries(formdata);
+  const result = updateProfileSchema.safeParse(data);
+  if (!result.success) {
+    const errs = z.flattenError(result.error).formErrors;
+    return {
+      success: false,
+      message: "Failed to update profile",
+      errors: errs,
+      data: null,
+      status: 400,
+      initialData: data,
+    };
+  }
+
+  type TProfileResponse = {
+    message: string;
+    data: TProfile;
+  };
+
+  const res = await apiClient.authenticated<TProfileResponse>("/profile", {
+    data: result.data,
+    method: "PATCH",
+  });
+
+  revalidatePath("/dashboard/accounts/edit");
+  if (!res.ok) {
+    return {
+      success: res.ok,
+      data: null,
+      errors: res.error,
+      message: "Failed to update profile",
+      status: res.status,
+      initialData: data,
+    };
+  } else {
+    return {
+      success: res.ok,
+      data: res.data,
+      errors: null,
+      message: "Successfully updated profile",
+      status: res.status,
+      initialData: data,
     };
   }
 }
