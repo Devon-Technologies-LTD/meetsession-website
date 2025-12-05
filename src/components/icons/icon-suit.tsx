7e19b94c54cc72588d23617151260da82236bfa7
Godswill Nasara <godswillnlm@gmail.com>
2025-12-05 18:27:47 +0100

    feat: create module for component icons

---
+++ src/components/icons/icon-suit.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+
+export function CustomBoltIcon({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={cn("size-6", className)}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+      />
+    </svg>
+  );
+}
