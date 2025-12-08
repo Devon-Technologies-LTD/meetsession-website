8fe2c6e0b2e06c8894cac1d2403a81e8e18b5e3e
Godswill Nasara <godswillnlm@gmail.com>
2025-12-08 14:07:34 +0100

    feat: add image placeholder for actual image

--- src/features/base/components/campus-program-ad.tsx
+++ src/features/base/components/campus-program-ad.tsx
@@ -25,11 +25,10 @@ import { Fragment } from "react";
 import Image from "next/image";
 import { useState } from "react";
 import { useIsMobile } from "@/hooks/use-mobile";
+// const isMobile = useIsMobile("desktop");
 */
 
 export function CampusProgramAd() {
-  // const isMobile = useIsMobile("desktop");
-
   return (
     <div
       className={cn(
@@ -41,127 +40,138 @@ export function CampusProgramAd() {
     >
       <div className={cn("flex flex-col gap-3 items-center")}>
         <ChipItem className="text-brand-green border-brand-green bg-brand-green/20 border-[1px]">
-          <CustomBoltIcon />
+          <CustomBoltIcon className="size-4 md:size-6" />
           <span>Join Our Community</span>
         </ChipItem>
 
-        <p className="text-6xl font-bold text-white font-dm-sans tracking-tighter">
+        <p className="text-3xl md:text-6xl font-bold text-white font-dm-sans tracking-tighter">
           MeetSession Campus Ambassador Program
         </p>
 
-        <p className="text-base md:text-xl text-neutral-100 max-w-200 w-full mx-auto">
+        <p className="text-sm md:text-xl text-neutral-300 md:text-neutral-100 max-w-200 w-full mx-auto">
           Lead the movement to transform how students capture, organise, and
           share knowledge. Join our global network of student leaders making
           real impact.
         </p>
       </div>
 
-      <div className="flex flex-col gap-5 md:gap-6 w-full max-w-screen-xl mx-auto">
-        <div className="flex items-start gap-7 md:gap-14">
-          <div className="w-1/2 h-full pt-14">
-            <div
-              className={cn(
-                "relative h-116 w-full flex flex-col justify-end",
-                "rounded-3xl p-6 overflow-hidden",
-                "border-[3px] border-brand-green/10",
-              )}
-            >
-              <div
-                className={cn(
-                  "flex items-center justify-between gap-10 w-full",
-                  "rounded-xl py-6 px-14",
-                  "bg-black/60 border border-brand-green/30",
-                )}
-              >
-                {metrics.map((metric, idx) => (
-                  <Fragment key={metric.id}>
-                    <div className="flex flex-col gap-2">
-                      <p className="text-2xl font-semibold text-brand-green-light">
-                        {metric.value}
-                      </p>
-                      <p className="text-xs">{metric.name}</p>
-                    </div>
-
-                    {idx < metrics.length - 1 && (
-                      <div className="h-14 w-px bg-brand-green/10" />
-                    )}
-                  </Fragment>
-                ))}
-              </div>
-            </div>
-          </div>
-
+      <div className="flex flex-col md:flex-row w-full items-start gap-5 md:gap-14 max-w-screen-xl mx-auto">
+        <div className={cn("w-full md:w-1/2 h-full py-0 md:pt-14")}>
           <div
             className={cn(
-              "flex flex-col gap-8",
-              "text-start items-start",
-              "w-1/2 h-fit",
+              "relative h-96 md:h-116 w-full",
+              "border-[2px] border-brand-green-extradark/80",
+              "rounded-3xl p-2 md:p-5 overflow-hidden",
+              "flex flex-col items-start md:items-center justify-end",
+              "after:absolute after:h-full after:w-full after:top-0 after:left-0",
+              "bg-[url('/image/ms-ambassador-bg.jpg')] bg-cover bg-center bg-no-repeat",
+              "after:bg-linear-to-b after:from-black/5 after:from-50% after:to-black/75",
             )}
           >
-            <p className="text-start text-3xl font-bold">
-              Why Become an Ambassador?
-            </p>
-
-            <div className="flex flex-col gap-4 w-full">
-              {reasons.map((reason) => (
-                <div
-                  key={reason.id}
-                  className={cn(
-                    "px-6 py-7 w-full",
-                    "rounded-2xl border border-brand-green-light/20",
-                    "flex gap-4 bg-white/5",
-                  )}
-                >
-                  <div className="bg-brand-green-light/20 text-brand-green-light/70 w-fit h-fit py-3 px-3 rounded-xl">
-                    {reason.icon}
-                  </div>
-
-                  <div className="text-start">
-                    <p className="font-semibold text-base md:text-lg">
-                      {reason.title}
-                    </p>
-                    <p className="text-xs md:text-sm text-neutral-400">
-                      {reason.description}
-                    </p>
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            <Link
-              href="/ambassador"
+            <div
               className={cn(
-                buttonVariants(),
-                "bg-brand-green hover:bg-brand-green-light",
-                "py-6 text-base text-medium w-full",
-                "flex items-center gap-2",
+                "bg-black/90 border border-brand-green/30",
+                "rounded-xl py-4 md:py-6 px-7 md:px-16 w-fit md:w-full",
+                "flex flex-col md:flex-row items-center justify-between gap-1 md:gap-10",
               )}
             >
-              <p>Apply to Become an Ambassador</p>
-              <ArrowRightIcon />
-            </Link>
+              {metrics.map((metric, idx) => (
+                <Fragment key={metric.id}>
+                  <div className="flex flex-col gap-0 md:gap-1">
+                    <p className="text-lg md:text-2xl font-semibold text-brand-green-light">
+                      {metric.value}
+                    </p>
+                    <p className="text-xs text-neutral-300">{metric.name}</p>
+                  </div>
 
-            <p className="text-neutral-300 text-xs md:text-sm text-center w-full">
-              Join students from universities worldwide making education more
-              collaborative
-            </p>
+                  {idx < metrics.length - 1 && (
+                    <div className="h-7 md:h-14 w-px bg-brand-green/20" />
+                  )}
+                </Fragment>
+              ))}
+            </div>
           </div>
+        </div>
+
+        <div
+          className={cn(
+            "flex flex-col gap-5 md:gap-8",
+            "text-start items-start",
+            "w-full md:w-1/2 h-fit",
+          )}
+        >
+          <p className="text-start text-lg md:text-3xl font-bold">
+            Why Become an Ambassador?
+          </p>
+
+          <div className="flex flex-col gap-2.5 md:gap-4 w-full">
+            {reasons.map((reason) => (
+              <div
+                key={reason.id}
+                className={cn(
+                  "flex gap-2 md:gap-4 bg-white/5",
+                  "px-3 md:px-6 py-4 md:py-7 w-full",
+                  "rounded-2xl border border-brand-green-light/20",
+                )}
+              >
+                <div
+                  className={cn(
+                    "w-fit h-fit py-1.5 md:py-3 px-1.5 md:px-3",
+                    "bg-brand-green-light/20 text-brand-green-light/70 rounded-md md:rounded-xl",
+                  )}
+                >
+                  {reason.icon}
+                </div>
+
+                <div className="text-start">
+                  <p className="font-semibold text-sm md:text-lg">
+                    {reason.title}
+                  </p>
+                  <p className="text-xs md:text-sm text-neutral-400">
+                    {reason.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <Link
+            href="/ambassador"
+            className={cn(
+              buttonVariants(),
+              "bg-brand-green hover:bg-brand-green-light",
+              "py-6 text-sm md:text-base text-medium w-full",
+              "flex items-center gap-1 md:gap-2",
+            )}
+          >
+            <p>Apply to Become an Ambassador</p>
+            <ArrowRightIcon />
+          </Link>
+
+          <p className="text-neutral-300 text-xs md:text-sm text-center w-full">
+            Join students from universities worldwide making education more
+            collaborative
+          </p>
         </div>
       </div>
 
       <div
         className={cn(
           "max-w-screen-xl w-full h-fit mx-auto",
-          "py-10 px-12 flex flex-col gap-12",
-          "rounded-3xl border border-white/10",
           "bg-linear-120 from-white/5 to-transparent",
+          "rounded-xl md:rounded-3xl border border-white/10",
+          "py-5 md:py-10 px-6 md:px-12 flex flex-col gap-6 md:gap-12",
         )}
       >
-        <p className="font-bold text-3xl">
+        <p className="font-bold text-lg md:text-3xl">
           What You&apos;ll Do as an Ambassador
         </p>
 
-        <div className={cn("flex justify-between gap-10")}>
+        <div
+          className={cn(
+            "flex flex-col md:flex-row justify-center md:justify-between gap-5 md:gap-10",
+          )}
+        >
           {expectations.map((exp) => (
             <ExpectationCard
               key={exp.id}
@@ -179,32 +189,32 @@ export function CampusProgramAd() {
 const reasons = [
   {
     id: 1,
-    icon: <UsersRoundIcon />,
+    icon: <UsersRoundIcon className="size-4 md:size-6" />,
     title: "Build Leadership",
     description: "Develop leadership skills and manage campus initiatives",
   },
   {
     id: 2,
-    icon: <AwardIcon />,
+    icon: <AwardIcon className="size-4 md:size-6" />,
     title: "Get Recognised",
     description: "Earn certificates and LinkedIn recommendations",
   },
   {
     id: 3,
-    icon: <TrendingUpIcon />,
+    icon: <TrendingUpIcon className="size-4 md:size-6" />,
     title: "Career Growth",
     description: "Access internships and mentorship opportunities",
   },
   {
     id: 4,
-    icon: <TargetIcon />,
+    icon: <TargetIcon className="size-4 md:size-6" />,
     title: "Make Impact",
     description: "Shape how students use MeetSession on your campus",
   },
 ];
 const metrics = [
   { id: 1, name: "Ambassadors", value: "500+" },
-  { id: 2, name: "Universities", value: "18+" },
+  { id: 2, name: "Universities", value: "85+" },
   { id: 3, name: "Students", value: "50k" },
 ];
 const expectations = [
@@ -240,11 +250,16 @@ function ExpectationCard({
   return (
     <Card className={cn("w-full", "bg-black/40", "border-none")}>
       <CardContent className="flex items-center justify-center">
-        <div className="bg-brand-green-extralight/10 rounded-xl w-fit h-fit py-2 px-2.5">
+        <div
+          className={cn(
+            "w-fit h-fit py-1 md:py-2 px-1.5 md:px-2.5",
+            "bg-brand-green-extralight/10 rounded-xl",
+          )}
+        >
           <p className="text-2xl font-bold text-brand-green">{id}</p>
         </div>
       </CardContent>
-      <CardHeader className="text-white flex flex-col items-center gap-4">
+      <CardHeader className="text-white flex flex-col items-center gap-2 md:gap-4">
         <CardTitle className="text-base md:text-lg">{title}</CardTitle>
         <CardDescription className="text-neutral-400">
           {description}
